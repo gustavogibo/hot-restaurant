5bbb2900041f3a8a6052b793fb7fc82fe5e67f08
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-05-31 13:16:30 -0700

    creating the server variables and arrays to hold data

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-alert("hello world");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var port = 3000;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.json());
+
+var tableList = [{}];
+
+var waitList = [{}];
+
